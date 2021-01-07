94dcb342d98a8751155145b3e7b1dc265fb69694
José Lucas <jltorquato12@gmail.com>
2021-01-07 09:58:17 -0300

    Criado endpoint de localizacao e testado no postman

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -21,6 +21,7 @@ from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from avaliacoes.api.viewsets import AvaliacoesViewSet
 from comentarios.api.viewsets import ComentariosViewSet
+from localizacao.api.viewsets import LocalizacaoViewSet
 
 
 router = routers.DefaultRouter()
@@ -28,6 +29,7 @@ router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
 router.register(r'avaliacoes', AvaliacoesViewSet)
 router.register(r'comentarios', ComentariosViewSet)
+router.register(r'localizacao', LocalizacaoViewSet)
 
 
 urlpatterns = [
